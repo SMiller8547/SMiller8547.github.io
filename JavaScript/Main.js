51a318f898dcb4010cacb5c4a4ce00397f937f40
SMiller <miller8547@gmail.com>
2018-10-15 12:13:57 -0700

    Full Site Push
    Making website Live

---
+++ JavaScript/Main.js
@@ -0,0 +1,8 @@
+
+function titleTransition() {
+	document.getElementById( 'coreTxtRoll' ).style.display = 'none';
+	document.getElementById( 'nameBorder' ).style.display = 'block';
+	/* document.getElementById( 'downloadBtn' ).style.visibility = 'visible';	 */
+}
+
+
